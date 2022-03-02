3de872dcd2d45bd907cd80f04878f415b4541f22
dvpappas <dvpappas89@gmail.com>
2022-03-02 15:23:04 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+
+This is a data collector created by ATHENA R.C. aiming to collect the data bumps of PubMed procided through ftp.
+
--- detect_and_download_new.py
+++ detect_and_download_new.py
@@ -2,15 +2,24 @@ __author__ = 'Dimitris Pappas'
 
 import os, json, gzip, traceback
 import urllib.request
+import argparse
 
-year        = '22'
+parser = argparse.ArgumentParser()
+parser.add_argument("--year",       type=int, default=2022, help="year",     required=False)
+parser.add_argument("--base_url",   type=str,               help="base_url of tgz files",            required=True)
+parser.add_argument("--out_dir",    type=str,               help="output root directory path.", required=True)
+
+args                = parser.parse_args()
+
+year                = str(args.year)[-2:]
+base_url            = args.base_url
+out_dir             = args.out_dir
 
 # base_url    = 'ftp://ftp.ncbi.nlm.nih.gov/pubmed/baseline/'
-base_url    = 'ftp://ftp.ncbi.nlm.nih.gov/pubmed/updatefiles/'
-diridiri    = './data/PUBMED/pubmed_updates_20{}/ftp.ncbi.nlm.nih.gov/pubmed/updatefiles/'.format(year)
+# base_url    = 'ftp://ftp.ncbi.nlm.nih.gov/pubmed/updatefiles/'
 
-if not os.path.exists(diridiri):
-    os.makedirs(diridiri)
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
 
 with urllib.request.urlopen(base_url) as response:
     html    = response.read().decode('utf-8')
@@ -21,7 +30,7 @@ with urllib.request.urlopen(base_url) as response:
             fpath       = line.split()[-1]
             i           = int(fpath.replace('pubmed{}n'.format(year),'').replace('.xml.gz', ''))
             file_url    = '{}{}'.format(base_url, fpath)
-            opath       = os.path.join(diridiri,fpath)
+            opath       = os.path.join(out_dir,fpath)
             if not os.path.exists(opath):
                 command = 'wget {} -O {}'.format(file_url, opath)
                 print(command)
