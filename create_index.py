62f185c0ab2cac58b1654c449d819fed6908632d
dvpappas <dvpappas89@gmail.com>
2022-03-02 15:15:46 +0200

    initial commit

---
+++ create_index.py
@@ -0,0 +1,281 @@
+
+from elasticsearch import Elasticsearch
+
+index   = 'pubmed_abstracts_0_1'
+map     = 'abstract_map_0_1'
+
+elastic_ips = ['127.0.0.1:9200']
+
+elastic_con = Elasticsearch(
+    elastic_ips,
+    verify_certs=True,
+    timeout=150,
+    max_retries=10,
+    retry_on_timeout=True
+)
+
+elastic_con.indices.delete(
+    index=index,
+    ignore=[400,404]
+)
+
+mapping = {
+    "mappings":{
+        map:{
+            "properties":{
+                ######### TEXT
+                'ArticleTitle'      : {
+                    "type"      : "text",
+                    "analyzer"  : "english",
+                },
+                'VernacularTitle'   : {
+                    "type"      : "text",
+                    "analyzer"  : "english",
+                },
+                'AbstractText'      : {
+                    "type"      : "text",
+                    "analyzer"  : "english",
+                },
+                ######### KEYWORDS
+                'Language'          : {
+                    "type": "keyword"
+                },
+                'pmid'              : {
+                    "type": "keyword"
+                },
+                'CitationSubset'    : {
+                    "type": "keyword"
+                },
+                ######### DATES
+                'ArticleDate'       : {
+                    "type"      : "date",
+                    "format"    : "yyy-MM-dd HH:mm:ss||yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||yyyy/MM/dd||dd/MM/yyyy||epoch_millis||EE MMM d HH:mm:ss Z yyyy"
+                },
+                'DateCompleted'     : {
+                    "type"      : "date",
+                    "format"    : "yyy-MM-dd HH:mm:ss||yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||yyyy/MM/dd||dd/MM/yyyy||epoch_millis||EE MMM d HH:mm:ss Z yyyy"
+                },
+                'DateRevised'       : {
+                    "type"      : "date",
+                    "format"    : "yyy-MM-dd HH:mm:ss||yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||yyyy/MM/dd||dd/MM/yyyy||epoch_millis||EE MMM d HH:mm:ss Z yyyy"
+                },
+                ######### NESTED
+                "Chemicals"         : {
+                    "properties": {
+                        "UI": {
+                            "type": "keyword"
+                        },
+                        "NameOfSubstance": {
+                            "type"      : "text",
+                            "analyzer"  : "english",
+                            "fields": {
+                                "raw": {
+                                    "type": "keyword"
+                                }
+                            }
+                        },
+                        "RegistryNumber": {
+                            "type": "keyword",
+                        },
+                    }
+                },
+                'OtherIDs'          : {
+                    "properties": {
+                        "id": {
+                            "type": "keyword"
+                        },
+                        "Source": {
+                            "type": "keyword"
+                        },
+                    }
+                },
+                'AbstractSegments'  : {
+                    "properties": {
+                        'NlmCategory' : {
+                            "type"      : "text",
+                            "analyzer"  : "english",
+                            "fields": {
+                                "raw": {
+                                    "type": "keyword"
+                                }
+                            }
+                        },
+                        'Label' : {
+                            "type"      : "text",
+                            "analyzer"  : "english",
+                            "fields": {
+                                "raw": {
+                                    "type": "keyword"
+                                }
+                            }
+                        },
+                        'text' : {
+                            "type"      : "text",
+                            "analyzer"  : "english"
+                        },
+                    }
+                },
+                'MeshHeadings'      : {
+                    "properties": {
+                        'UI' : {
+                            "type": "keyword"
+                        },
+                        'MajorTopicYN' : {
+                            "type": "keyword"
+                        },
+                        'Type' : {
+                            "type": "keyword"
+                        },
+                        'text' : {
+                            "type"      : "text",
+                            "analyzer"  : "english",
+                            "fields": {
+                                "raw": {
+                                    "type": "keyword"
+                                }
+                            }
+                        },
+                        'Label' : {
+                            "type"      : "text",
+                            "analyzer"  : "english",
+                            "fields": {
+                                "raw": {
+                                    "type": "keyword"
+                                }
+                            }
+                        },
+                    }
+                },
+                'Grants'            : {
+                    "properties": {
+                        'Country' : {
+                            "type": "keyword"
+                        },
+                        'GrantID' : {
+                            "type": "keyword"
+                        },
+                        'Agency' : {
+                            "type": "keyword"
+                        },
+                    }
+                },
+                "Keywords"          : {
+                    "type"      : "text",
+                    "analyzer"  : "english",
+                    "fields": {
+                        "raw": {
+                            "type": "keyword"
+                        }
+                    }
+                },
+                'SupplMeshList'     : {
+                    "properties": {
+                        'text' : {
+                            "type"      : "text",
+                            "analyzer"  : "english",
+                            "fields": {
+                                "raw": {
+                                    "type": "keyword"
+                                }
+                            }
+                        },
+                        'GrantID' : {
+                            "type": "keyword"
+                        },
+                        'Agency' : {
+                            "type": "keyword"
+                        },
+                    }
+                },
+                'Journal'           : {
+                    "properties": {
+                        'Title' : {
+                            "type"      : "text",
+                            "analyzer"  : "english",
+                            "fields": {
+                                "raw": {
+                                    "type": "keyword"
+                                }
+                            }
+                        },
+                        'ISOAbbreviation' : {
+                            "type": "keyword"
+                        },
+                        'ISSN' : {
+                            "type": "keyword"
+                        },
+                        'JournalIssue' : {
+                            "properties": {
+                                'Volume' : {
+                                    "type": "keyword"
+                                },
+                                'Issue' : {
+                                    "type": "keyword"
+                                },
+                                # 'PubDate' : {
+                                #     "type"      : "date",
+                                #     "format"    : "dd-MM-yyyy||yyy-MM-dd HH:mm:ss||yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||yyyy/MM/dd||epoch_millis||EE MMM d HH:mm:ss Z yyyy"
+                                # },
+                                'PubDate' : {
+                                    "type": "keyword"
+                                },
+                            }
+                        },
+                    }
+                },
+                'PublicationType'   : {
+                    "properties": {
+                        'Type': {
+                            "type": "text",
+                            "analyzer": "english",
+                            "fields": {
+                                "raw": {
+                                    "type": "keyword"
+                                }
+                            }
+                        },
+                        'UI': {
+                            "type": "keyword"
+                        }
+                    }
+                },
+                'ELocationIDs'      : {
+                    "properties": {
+                        'value': {
+                            "type": "keyword"
+                        },
+                        'ValidYN': {
+                            "type": "keyword"
+                        },
+                        'EIdType': {
+                            "type": "keyword"
+                        }
+                    }
+                },
+                'DataBankList'      : {
+                    "properties": {
+                        'DataBankName': {
+                            "type": "text",
+                            "analyzer": "english",
+                            "fields": {
+                                "raw": {
+                                    "type": "keyword"
+                                }
+                            }
+                        },
+                        'AccessionNumbers': {
+                            "type": "keyword"
+                        }
+                    }
+                },
+            }
+        }
+    }
+}
+
+print(elastic_con.indices.create(index = index, ignore=400, body=mapping))
+
+
+
+
+
---
+++ detect_and_download_new.py
@@ -0,0 +1,39 @@
+
+import os, json, gzip, traceback
+import urllib.request
+
+year        = '22'
+
+# base_url    = 'ftp://ftp.ncbi.nlm.nih.gov/pubmed/baseline/'
+base_url    = 'ftp://ftp.ncbi.nlm.nih.gov/pubmed/updatefiles/'
+diridiri    = '/media/dpappas/dpappas_data/PUBMED/pubmed_updates_20{}/ftp.ncbi.nlm.nih.gov/pubmed/updatefiles/'.format(year)
+
+if not os.path.exists(diridiri):
+    os.makedirs(diridiri)
+
+with urllib.request.urlopen(base_url) as response:
+    html    = response.read().decode('utf-8')
+    lines   = html.split('\r\n')
+    for line in lines:
+        line = line.strip()
+        if(line.endswith('.xml.gz')):
+            fpath       = line.split()[-1]
+            i           = int(fpath.replace('pubmed{}n'.format(year),'').replace('.xml.gz', ''))
+            file_url    = '{}{}'.format(base_url, fpath)
+            opath       = os.path.join(diridiri,fpath)
+            if not os.path.exists(opath):
+                command = 'wget {} -O {}'.format(file_url, opath)
+                print(command)
+                os.system(command)
+                while(True):
+                    try:
+                        infile  = gzip.open(opath)
+                        content = infile.read()
+                        break
+                    except:
+                        command = 'wget {} -O {}'.format(file_url, opath)
+                        print(command)
+                        os.system(command)
+
+# python3.6 detect_and_download_new.py
+
